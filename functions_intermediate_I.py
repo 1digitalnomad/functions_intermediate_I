131c41f52a2c0633441076a874723468a203b0b1
Raul <Takethefirststep@Rauls-iMac.attlocal.net>
2018-10-21 14:55:17 -0700

    Random number generator.

---
+++ functions_intermediate_I.py
@@ -0,0 +1,22 @@
+import random
+
+# def randInt():
+#     print (int(random.random()*50))
+
+# randInt()
+
+# def randInt(max=50):
+#     print(int(random.random()*max))
+
+# randInt()
+
+
+# def randInt(min=50):
+#     print (int(random.randint(min,100)))
+
+# randInt()
+
+def randInt(min=50, max=500):
+    print(random.randint(min,max))
+
+randInt()
